39c51e7e75740106a197a60f8f03501692a6f87e
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-04-27 22:49:24 +0545

    Tuples

---
+++ src/tuples.rs
@@ -0,0 +1,7 @@
+// Tuples group together values of different types
+// Max 12 elements
+pub fn run() {
+    let person: (&str, &str, i8) = ("Samyog", "Dang", 21);
+
+    println!("{} is from {} and is {}", person.0, person.1, person.2);
+}
